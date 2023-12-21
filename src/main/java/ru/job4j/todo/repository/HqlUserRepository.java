7fce2cdb968e51c895e8ba4bba7474c70365c88e
PavelChupin <pavelchupin94@yandex.ru>
2023-12-21 18:19:56 +0300

    5. Аутентификация в проекте TODO. Фикс.

--- src/main/java/ru/job4j/todo/repository/HqlUserRepository.java
+++ src/main/java/ru/job4j/todo/repository/HqlUserRepository.java
@@ -4,11 +4,8 @@ import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
-import ru.job4j.todo.model.Task;
 import ru.job4j.todo.model.User;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -24,30 +21,31 @@ public class HqlUserRepository implements UserRepository  {
             session.beginTransaction();
             session.save(user);
             session.getTransaction().commit();
+            return Optional.of(user);
         } catch (Exception e) {
             session.getTransaction().rollback();
         } finally {
             session.close();
         }
-        return Optional.of(user);
+        return Optional.empty();
     }
 
     @Override
     public Optional<User> findByLoginAndPassword(String login, String password) {
         Session session = sf.openSession();
-        List<User> query = Collections.emptyList();
+        Optional<User> query = Optional.empty();
         try {
             session.beginTransaction();
             query = session.createQuery(
-                            "from User WHERE login = :fLog and password = :fPas", User.class)
-                    .setParameter("fLog", login)
-                    .setParameter("fPas", password).list();
+                    "from User WHERE login = :fLog and password = :fPas", User.class)
+            .setParameter("fLog", login)
+            .setParameter("fPas", password).uniqueResultOptional();
             session.getTransaction().commit();
         } catch (Exception e) {
             session.getTransaction().rollback();
         } finally {
             session.close();
         }
-        return Optional.of(query.get(0));
+        return query;
     }
 }
